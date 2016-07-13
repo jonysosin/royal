03f50e5d3bc5859d9916dd98b6b04c24f0cd09e7
Jonathan Sosin <jonysosin@gmail.com>
2016-07-13 00:21:41 -0300

    Slider

--- js/main.js
+++ js/main.js
@@ -7,6 +7,86 @@ $(document).ready(function(e) {
         checkSectionScroll();
         console.log(scrollY);
     });
+
+    $("#owl-example").owlCarousel({
+
+        // Most important owl features
+        items : 5,
+        itemsCustom : false,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,2],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
+        singleItem : false,
+        itemsScaleUp : false,
+
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+        rewindSpeed : 1000,
+
+        //Autoplay
+        autoPlay : true,
+        stopOnHover : false,
+
+        // Navigation
+        navigation : true,
+        navigationText : [
+            "<span class='prev-btn'><i class='fa fa-chevron-left'></i></span>",
+            "<span class='next-btn'><i class='fa fa-chevron-right'></i></span>",
+        ],
+        rewindNav : true,
+        scrollPerPage : false,
+
+        //Pagination
+        pagination : false,
+        paginationNumbers: false,
+
+        // Responsive
+        responsive: true,
+        responsiveRefreshRate : 200,
+        responsiveBaseWidth: window,
+
+        // CSS Styles
+        baseClass : "owl-carousel",
+        theme : "owl-theme",
+
+        //Lazy load
+        lazyLoad : false,
+        lazyFollow : true,
+        lazyEffect : "fade",
+
+        //Auto height
+        autoHeight : false,
+
+        //JSON
+        jsonPath : false,
+        jsonSuccess : false,
+
+        //Mouse Events
+        dragBeforeAnimFinish : true,
+        mouseDrag : true,
+        touchDrag : true,
+
+        //Transitions
+        transitionStyle : false,
+
+        // Other
+        addClassActive : false,
+
+        //Callbacks
+        beforeUpdate : false,
+        afterUpdate : false,
+        beforeInit: false,
+        afterInit: false,
+        beforeMove: false,
+        afterMove: false,
+        afterAction: false,
+        startDragging : false,
+        afterLazyLoad : false
+
+    })
 });
 
 function checkSectionScroll() {
